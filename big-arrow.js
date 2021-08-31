cec3f4c022f2fe2f6229447a1bd0b0c0afd06351
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-31 20:05:13 +0600

    big arrow function

---
+++ big-arrow.js
@@ -0,0 +1,30 @@
+//multiple parameter
+const add = (num1, num2) => num1 + num2;
+const sum = add(22, 90);
+
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+const result = multiply(4, 12, 3);
+
+//only one parameter
+const tenTimes = (num) => num * 10;
+const output = tenTimes(17);
+
+const fiveTimes = num => num * 5;
+const value = fiveTimes(17);
+
+//No parameter
+const getName = () => 'Brandon Sam';
+const name = getName()
+console.log(name);
+
+//multiline arrow function
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    const output = result * 5;
+    return output;
+}
+
+const total = doMath(12, 5);
+console.log(total);
