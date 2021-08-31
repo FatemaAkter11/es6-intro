6180d762c980e00fe747d97f0cf42ba5dacbae57
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-31 20:01:08 +0600

    default parameter for not provided values

---
+++ default-parameter.js
@@ -0,0 +1,19 @@
+function add(num1, num2 = 33) {
+    // option 2
+    // num2 = num2 || 0;
+    // option 1
+    // if (num2 == undefined) {
+    //     num2 = 0;
+    // }
+    const total = num1 + num2;
+    return total;
+}
+const result = add(15);
+console.log(result);
+
+function fullName(first, last = 'Chowdhury') {
+    const name = first + ' ' + last;
+    return name;
+}
+const myName = fullName('Ali', 'Akbar');
+// console.log(myName);
